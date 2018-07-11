fa493b43fd46f7f80710d03782010e93c9a2eade
Kira Hartness <kirahartness@gmail.com>
2018-07-11 02:20:49 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -2,6 +2,7 @@
 # Every line should have one instruction using puts.
 # The first instruction is correct. 
 
+"How To Do The Macarena:"
 # Step 1: GOOD
 puts "Step 1: Right arm out in front of you, palm facing down."
 
